8819f9a00e8497c890d7e1c3b1667efe39f9ddfc
YuseonChoi <chldbtjs74@gmail.com>
2021-10-08 17:01:53 +0900

    [ADD] product list

--- productapp/models.py
+++ productapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)                  # 상품 이름
+    price = models.IntegerField(default=0)                  # 상품 가격
+    content = models.TextField()                            # 상품 설명
+    upload_date = models.DateTimeField(auto_now_add=True)    # 상품 게시일
+    # writer : 추후 작성 예정
+
+    def __str__(self):
+        return f'[{self.pk}] {self.name}'
--- productapp/views.py
+++ productapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+
+from productapp.models import Product
+
+
+# class ProductList(ListView):
+#     model = Product
+
+def index(request):
+    # 가장 최근에 등록된 상품부터 나열
+    products = Product.objects.all().order_by('-pk')
+
+    return render(
+        request,
+        'productapp/index.html',
+        {
+            'products': products,
+        }
+    )
