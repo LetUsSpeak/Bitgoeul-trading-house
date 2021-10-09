4b026a5203ee7ca049449a610140957e513f40be
YuseonChoi <chldbtjs74@gmail.com>
2021-10-10 01:58:51 +0900

    [UPD] product list design

--- productapp/models.py
+++ productapp/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=30)                  # 상품 이름
     price = models.IntegerField(default=0)                  # 상품 가격
     content = models.TextField()                            # 상품 설명
-    upload_date = models.DateTimeField(auto_now_add=True)    # 상품 게시일
+    upload_date = models.DateTimeField(auto_now_add=True)   # 상품 게시일
     # writer : 추후 작성 예정
 
     def __str__(self):
