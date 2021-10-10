2420c34f26dc7e6c0ae96062e174818efd000c91
YuseonChoi <chldbtjs74@gmail.com>
2021-10-11 04:02:10 +0900

    [ADD] event detail page

--- eventapp/models.py
+++ eventapp/models.py
@@ -7,6 +7,8 @@ from django.urls import reverse
 class Event(models.Model):
     name = models.CharField(max_length=200)
     when = models.DateTimeField()
+    info = models.TextField(blank=True)
+    market_image = models.ImageField(upload_to='eventapp/images/%Y/%m/', blank=True)
 
     def __str__(self):
         return str(self.name)
--- marketapp/views.py
+++ marketapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def landing(request):
     return render(
         request,
