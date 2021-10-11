05286cad304033221ecd96da64f144e6f08f7b3d
YuseonChoi <chldbtjs74@gmail.com>
2021-10-12 00:47:28 +0900

    [ADD] Category model

--- productapp/admin.py
+++ productapp/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from productapp.models import Product
+from productapp.models import Product, Category
 
 admin.site.register(Product)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name', )}
+
+admin.site.register(Category, CategoryAdmin)
--- productapp/models.py
+++ productapp/models.py
@@ -2,11 +2,23 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
 class Product(models.Model):
     name = models.CharField(max_length=30)                  # 상품 이름
     price = models.IntegerField(default=0)                  # 상품 가격
     market_name = models.CharField(max_length=30, blank=True)
     head_image = models.ImageField(upload_to='productapp/images/%Y/%m/%d/', blank=True)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
     # writer : 추후 작성 예정
 
     def __str__(self):
@@ -14,3 +26,5 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return f'/product/{self.pk}'
+
+
