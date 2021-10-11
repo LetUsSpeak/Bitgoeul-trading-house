ff2aad0f7a9b80be19dc33d8a321990c41288ae5
kovus380 <kosaf888@gmail.com>
2021-10-11 20:12:55 +0900

    product create

--- productapp/forms.py
+++ productapp/forms.py
@@ -5,10 +5,6 @@ from productapp.models import Product
 
 
 class ProductCreationForm(ModelForm):
-    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable',
-                                                           'style': 'text-align: left'
-                                                                    'min-height: 10rem'}))
-
     class Meta:
         model = Product
         fields = ['name', 'price', 'market_name', 'head_image']
