f21ee6d2393e65ea11b4143001a7f9779398a0c3
kovus380 <kosaf888@gmail.com>
2021-10-12 19:00:00 +0900

    kakao send message

--- marketapp/urls.py
+++ marketapp/urls.py
@@ -6,4 +6,5 @@ app_name = 'marketapp'
 urlpatterns = [
     path('', views.landing, name="main"),
     path('metaverse/', views.metaverse, name="metaverse"),
+    path('kakao/', views.kakao, name='kakao')
 ]
--- marketapp/views.py
+++ marketapp/views.py
@@ -1,4 +1,8 @@
+import json
+
+import requests
 from django.shortcuts import render
+
 
 # Create your views here.
 
@@ -9,8 +13,32 @@ def landing(request):
         'marketapp/landing.html'
     )
 
+
 def metaverse(request):
     return render(
         request,
         'marketapp/metaverse.html'
     )
+
+
+def kakao(request):
+    with open('data/token.txt', encoding='utf-8') as txtfile:
+        for row in txtfile.readlines():
+            token = row
+    url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
+
+    headers = {
+        "Authorization": "Bearer " + token
+    }
+
+    data = {
+        "template_object": json.dumps({"object_type": "text",
+                                       "text": "🎊 빛고을상회 🎊 에 오신 것을 환영합니다",
+                                       "link": {
+                                           "web_url": "http://127.0.0.1:9000/"
+                                       }
+                                       })
+    }
+    response = requests.post(url, headers=headers, data=data)
+
+    return render(request, 'marketapp/landing.html')
